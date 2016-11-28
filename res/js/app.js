3c058736ac12fc10a879f330dc715880a0b1247c
aloo <aloocs@Gmail.com>
2016-11-28 11:27:10 +0800

    加入了tab的状态保存

--- res/js/app.js
+++ res/js/app.js
@@ -1,6 +1,16 @@
 $(function(){
-    console.log("ok");
     $('#image-con').lightGallery({
         thumbnail:true
     });
+
+// store the currently selected tab in the hash value
+    $("ul.nav-tabs > li > a").click("shown.bs.tab", function(e) {
+
+        var id = $(e.target).attr("href").substr(1);
+        window.location.hash = id;
+    });
+
+// on load of the page: switch to the currently selected tab
+    var hash = window.location.hash;
+    $('.nav-tabs a[href="' + hash + '"]').tab('show');
 });
